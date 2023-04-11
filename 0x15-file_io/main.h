66a8aee00c293280365fbf8cfe8658ed033b9afa
Moses Tinuoye <tinuoyemosesade@gmail.com>
2023-04-11 18:56:07 +0200

    elf header and update header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
+#include <elf.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+/* elf_header.c */
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
 
 #endif /* MAIN_H */
+
