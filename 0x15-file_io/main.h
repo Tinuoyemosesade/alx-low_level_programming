da579d7cdfea7208a4c9898ccf7a1950478243ad
Moses Tinuoye <tinuoyemosesade@gmail.com>
2023-04-11 18:49:18 +0200

    update header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,8 +3,7 @@
 
 #include <unistd.h>
 #include <fcntl.h>
-
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
+#include <stdio.h>
+#include <stdlib.h>
 
 #endif /* MAIN_H */
