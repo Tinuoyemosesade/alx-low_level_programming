bbac6294f5c8cf978d69a95ab0d86e85346162e9
Moses Tinuoye <63667580+Tinuoyemosesade@users.noreply.github.com>
2023-05-11 18:34:08 +0200

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,8 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error(char *msg);
+void print_elf_header(Elf64_Ehdr *header);
+void read_elf_header(int fd, Elf64_Ehdr *header);
 
 #endif
